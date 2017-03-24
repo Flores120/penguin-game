e350f259bce11de351437584f9c82b1d3b923155
Alvaro Flores <alvaro.flores01@gmail.com>
2017-03-24 13:46:18 -0700

    adds new function to yeti

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
  function yetiAlert(){
    var modal = document.getElementById("alert");
+   var yeti = document.getElementById("yeti");
+   yeti.style.backgroundImage = "url('./img/yeti.png')";
    modal.style.display = "block";
 };
 
 function closeAlert(){
   var modal = document.getElementById("alert");
+  var yeti = document.getElementById("yeti");
   modal.style.display = "none";
+  yeti.style.backgroundImage = "url('./img/mound_9.png')";
 }
