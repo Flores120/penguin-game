70bfcbee0d443c4baa1e11c1abbecf44873efa67
Alvaro Flores <alvaro.flores01@gmail.com>
2017-03-24 12:18:08 -0700

    adds yeti and more functionality

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+ function yetiAlert(){
+   var modal = document.getElementById("alert");
+   modal.style.display = "block";
+};
+
+function closeAlert(){
+  var modal = document.getElementById("alert");
+  modal.style.display = "none";
+}
